03d04741cd22566a3472837169d311a77a77f5c4
ru-rocker <ru.rocker81@gmail.com>
2020-08-10 16:34:03 +0700

    optimize imports

--- src/main/java/com/rurocker/example/kafkastream/dto/EmploymentHistoryAggregationDto.java
+++ src/main/java/com/rurocker/example/kafkastream/dto/EmploymentHistoryAggregationDto.java
@@ -2,9 +2,7 @@ package com.rurocker.example.kafkastream.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class EmploymentHistoryAggregationDto {
