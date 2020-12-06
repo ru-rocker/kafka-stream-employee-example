f1519b848892461da46c7586e8c9c25e63b59616
ru-rocker <ru.rocker81@gmail.com>
2020-12-06 15:52:09 +0700

    add testing libs

---
+++ src/main/java/com/rurocker/example/kafkastream/topology/WordCountTopology.java
@@ -0,0 +1,6 @@
+package com.rurocker.example.kafkastream.topology;/**
+ * @author Ricky Martaputra 
+ * Created on 06-Dec-2020 2:45 PM
+ */
+public class WordCountTopology {
+}
---
+++ src/test/java/com/rurocker/example/kafkastream/topology/WordCountTopologyTest.java
@@ -0,0 +1,6 @@
+package com.rurocker.example.kafkastream.topology;/**
+ * @author Ricky Martaputra 
+ * Created on 06-Dec-2020 2:48 PM
+ */
+public class WordCountTopologyTest {
+}
