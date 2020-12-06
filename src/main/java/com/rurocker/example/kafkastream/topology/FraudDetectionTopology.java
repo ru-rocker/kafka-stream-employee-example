5b5eaac12f717c77e3c3cbe0e449fe6666a3057c
ru-rocker <ru.rocker81@gmail.com>
2020-12-06 19:28:59 +0700

    Set use case for fraud detection

---
+++ src/main/java/com/rurocker/example/kafkastream/topology/FraudDetectionTopology.java
@@ -0,0 +1,17 @@
+package com.rurocker.example.kafkastream.topology;
+
+/**
+ * This is an example to detect if a credit card transactions exceed
+ * certain limit during specific period.
+ *
+ * User story:
+ * As a credit card owner with number XXX, I want to be notified if my transactions
+ * exceed $1000 in the 5 minutes duration.
+ *
+ * @author ru-rocker
+ * Created on 06-Dec-2020 7:21 PM
+ */
+public class FraudDetectionTopology {
+
+    // TODO: later
+}
