542092caecaa466ed8b30e8fd8426023743d688b
ru-rocker <ru.rocker81@gmail.com>
2020-12-06 15:52:35 +0700

    simple test: word count topology

--- src/main/java/com/rurocker/example/kafkastream/topology/WordCountTopology.java
+++ src/main/java/com/rurocker/example/kafkastream/topology/WordCountTopology.java
@@ -1,6 +1,31 @@
-package com.rurocker.example.kafkastream.topology;/**
- * @author Ricky Martaputra 
+package com.rurocker.example.kafkastream.topology;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+
+/**
+ * @author ru-rocker
  * Created on 06-Dec-2020 2:45 PM
  */
 public class WordCountTopology {
+
+    public void createTopology(StreamsBuilder builder) {
+        final KStream<String, String> textLines = builder
+                .stream("streams-plaintext-input",
+                        Consumed.with(Serdes.String(), Serdes.String()));
+
+        textLines
+            .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+            .groupBy((key, value) -> value)
+            .count(Materialized.as("WordCount"))
+            .toStream()
+            .to("streams-wordcount-output",
+                    Produced.with(Serdes.String(), Serdes.Long()));
+    }
 }
--- src/test/java/com/rurocker/example/kafkastream/topology/WordCountTopologyTest.java
+++ src/test/java/com/rurocker/example/kafkastream/topology/WordCountTopologyTest.java
@@ -1,6 +1,98 @@
-package com.rurocker.example.kafkastream.topology;/**
- * @author Ricky Martaputra 
+package com.rurocker.example.kafkastream.topology;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.function.BiConsumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author ru-rocker
  * Created on 06-Dec-2020 2:48 PM
  */
 public class WordCountTopologyTest {
+
+    private TopologyTestDriver testDriver;
+    private TestInputTopic<String, String> plainTextInput;
+    private TestOutputTopic<String, Long> wordCountOutput;
+    private final Serde<String> stringSerde = new Serdes.StringSerde();
+    private final Serde<Long> longSerde = new Serdes.LongSerde();
+
+    @BeforeEach
+    public void init() {
+
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "app-id");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, stringSerde.getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, stringSerde.getClass().getName());
+
+        WordCountTopology wordCountTopology = new WordCountTopology();
+        final StreamsBuilder builder = new StreamsBuilder();
+        wordCountTopology.createTopology(builder);
+        final Topology topology = builder.build();
+
+        testDriver = new TopologyTestDriver(topology, props);
+        plainTextInput = testDriver.createInputTopic("streams-plaintext-input", stringSerde.serializer(),
+                stringSerde.serializer());
+        wordCountOutput = testDriver.createOutputTopic("streams-wordcount-output", stringSerde.deserializer(),
+                longSerde.deserializer());
+    }
+
+    @Test
+    @DisplayName("Test word count streams")
+    public void testWordCountStream() {
+        String text1 = "Welcome to kafka streams";
+        String text2 = "Kafka streams is great";
+        String text3 = "Welcome back";
+
+        Map<String,Long> expected = Map.of(
+                "welcome", 2L,
+                "to", 1L,
+                "kafka", 2L,
+                "streams", 2L,
+                "is", 1L,
+                "great", 1L,
+                "back", 1L
+        );
+
+        plainTextInput.pipeInput(null,text1);
+        plainTextInput.pipeInput(null,text2);
+        plainTextInput.pipeInput(null,text3);
+
+        assertThat(wordCountOutput.isEmpty()).isFalse();
+
+        Map<String, Long> result = new HashMap<>();
+        while(!wordCountOutput.isEmpty()) {
+            final KeyValue<String, Long> kv = wordCountOutput.readKeyValue();
+            result.put(kv.key, kv.value);
+        }
+        assertThat(result).containsExactlyInAnyOrderEntriesOf(expected);
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        testDriver.close();
+        FileUtils.deleteDirectory(new File("/tmp/kafka-streams/app-id"));
+    }
+
 }
